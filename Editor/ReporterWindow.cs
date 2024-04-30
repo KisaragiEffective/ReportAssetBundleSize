f44e8eb4f36cfba4fdffd5c21f58461c76b4496a
Kisaragi Marine <kisaragi.effective@gmail.com>
2024-04-30 20:50:08 +0900

    initial commit

---
+++ Editor/ReporterWindow.cs
@@ -0,0 +1,119 @@
+#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+using VRC.SDK3.Avatars.Components;
+using VRC.SDK3A.Editor;
+
+namespace ReportAssetBundleSize.Editor
+{
+    public class ReporterWindow : EditorWindow
+    {
+        [MenuItem("Tools/ReportAssetBundleSize")]
+        private static void ShowEditorWindow()
+        {
+            EditorWindow wnd = GetWindow<ReporterWindow>();
+            wnd.titleContent = new GUIContent("Reporter");
+        }
+
+        private void CreateGUI()
+        {
+            var buildAvatarToCheck = new ObjectField("Game Object to check")
+                { objectType = typeof(VRCAvatarDescriptor) };
+            var callPreBuiltHook = new Toggle("Call VRCSDK hooks")
+                { tooltip = "do you want NDMF or like-wise tools to run?", value = false };
+            callPreBuiltHook.SetEnabled(false);
+            var compressedSize = new TextField("Compressed size") { value = "???", isReadOnly = true };
+            var decompressedSize = new TextField("Decompressed size") { value = "???", isReadOnly = true };
+
+            var b = new Button(ClickEvent);
+            var q = new HelpBox("調べるためにはアバターをセットしてください", HelpBoxMessageType.Error)
+            {
+                style =
+                {
+                    display = DisplayStyle.None
+                }
+            };
+            rootVisualElement.Add(buildAvatarToCheck);
+            rootVisualElement.Add(compressedSize);
+            rootVisualElement.Add(decompressedSize);
+            rootVisualElement.Add(callPreBuiltHook);
+            // TODO: toggle visibility
+            rootVisualElement.Add(q);
+            b.Add(new Label("ビルド"));
+            b.SetEnabled(true);
+            rootVisualElement.Add(b);
+            return;
+
+            void ClickEvent()
+            {
+                if (callPreBuiltHook.value)
+                {
+                    // blocker: ボタンのクロージャがasyncを受け取るように見えないからスピンロックしようとしたらエディタのメインスレッドをブロックしてしまって死ぬ
+                    // しかし、VRCSDKのbuildもbuildAndUploadもTaskを返してくる :blob_frustrated2:
+                    throw new NotImplementedException("VRCSDK call integration is not supported yet");
+                }
+                
+                if (buildAvatarToCheck.value is null)
+                {
+                    throw new Exception("please set Avatar to check");
+                }
+
+                Debug.Log("build");
+                var temporaryDirectory = "Assets/ZZZ_GeneratedAssets";
+                if (!Directory.Exists(temporaryDirectory))
+                {
+                    Directory.CreateDirectory(temporaryDirectory);
+                }
+
+                var ad = (buildAvatarToCheck.value as VRCAvatarDescriptor)!;
+                // パスが`**/*.prefab`の形式じゃないと例外を吐く。知るかよ！
+                var localPath = AssetDatabase.GenerateUniqueAssetPath(temporaryDirectory + "/" + ad.gameObject.name + ".prefab");
+                {
+                    PrefabUtility.SaveAsPrefabAsset(ad.gameObject, localPath, out var s);
+                    if (!s)
+                    {
+                        Debug.LogError("persistence failed!");
+                        return;
+                    }
+                }
+
+                Build("Compressed", BuildAssetBundleOptions.ChunkBasedCompression, in compressedSize);
+                // TODO: だいぶ頭悪い可能性がある
+                Build("Decompressed", BuildAssetBundleOptions.UncompressedAssetBundle, in decompressedSize);
+                return;
+
+                void Build(string assetBundleName, BuildAssetBundleOptions opts, in TextField write)
+                {
+                    var bundlesToBuild = new List<AssetBundleBuild> { new() { assetNames = new[] { localPath }, assetBundleName = assetBundleName } };
+
+                    // TODO: 多分BuildTargetは関係ないと思う。思いたい。だれかよろしく！ｗ
+                    var compressedManifest = BuildPipeline.BuildAssetBundles(
+                        temporaryDirectory, bundlesToBuild.ToArray(), opts, BuildTarget.StandaloneWindows64
+                    );
+
+                    if (compressedManifest == null)
+                    {
+                        Debug.LogError($"Build ({assetBundleName}) failed, see Console and Editor log for details");
+                        return;
+                    }
+
+                    Debug.Log($"Build finished ({assetBundleName})");
+                    var builtAssetBundleRelativePath = compressedManifest.GetAllAssetBundles()!.Single();
+                    var projectRelativePath = temporaryDirectory + "/" + builtAssetBundleRelativePath;
+                    var len = new FileInfo(projectRelativePath).Length;
+                    write.value = $"{len} bytes ({len / 1024.0 / 1024.0:F2} MiB / {len / 1000.0 / 1000.0:F2} MB)";
+                }
+            }
+        }
+    }
+}
+#endif
